34c20c0fcd89ed21bbe1374b4c822e744911e708
yuhua.zhao <yuhua.zhao@happyelements.com>
2023-09-05 14:26:57 +0800

    update config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -3,7 +3,7 @@ return {
     "Shatur/neovim-ayu",
     lazy = false,
     config = function ()
-      vim.cmd[[colorscheme ayu-dark]]
+      vim.cmd[[colorscheme ayu-mirage]]
     end
   },
   { "wfxr/minimap.vim" },
